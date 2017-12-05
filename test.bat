83c97e86888516af01b52f2857d28a2a145a7b52
Simon Knott <simoknott@gmail.com>
2017-12-05 18:15:05 +0100

    Update Windows Script

--- test.bat
+++ test.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
 
-xcopy /y .\input .\src\main\java
+xcopy /Y .\mock .\src\main\java
+xcopy /Y .\input .\src\main\java
 del .\src\main\java\README.md
 
 call gradlew.bat test
 call gradlew.bat showReport
+
+xcopy /Y .\mock .\src\main\java
