71f6907575113868c22334edd478ca49b1202d3d
Simon Knott <simoknott@gmail.com>
2017-12-20 11:01:35 +0100

    Modify Test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-cp mock/* src/main/java
-cp input/* src/main/java
+test -d "src/main/java" || mkdir -p "src/main/java" && cp mock/* src/main/java
+test -d "src/main/java" || mkdir -p "src/main/java" && cp input/* src/main/java
 
 rm src/main/java/README.md
 
